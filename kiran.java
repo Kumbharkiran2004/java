708bea69d2f8cb7ad9ff81256cfa4a49ce9f8410
Kumbharkiran2004 <kumbharkiran095@gmail.com>
2026-02-25 11:17:48 +0530

    Update kiran.java

--- kiran.java
+++ kiran.java
@@ -1,27 +1,5 @@
-import java.util.Scanner;
-
-public class NameGreeter {
+public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String name = "";
-
-        System.out.println("--- Welcome to the Infinite Greeter ---");
-        System.out.println("(Type 'exit' to stop the program)");
-
-        // This loop runs as long as the user doesn't type "exit"
-        while (!name.equalsIgnoreCase("exit")) {
-            System.out.print("\nPlease enter a name: ");
-            name = input.nextLine();
-
-            if (name.equalsIgnoreCase("exit")) {
-                System.out.println("Goodbye! Closing program...");
-            } else if (name.trim().isEmpty()) {
-                System.out.println("You didn't type anything! Try again.");
-            } else {
-                System.out.println("Hello, " + name + "! Hope you're having a great day.");
-            }
-        }
-
-        input.close();
+        System.out.println("Kiran");
     }
 }
